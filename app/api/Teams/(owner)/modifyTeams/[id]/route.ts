1276ac1eb704d9e1e4ce7072faae906575ada1d9
cmendez-2018307 <108351423+cmendez-2018307@users.noreply.github.com>
2023-05-29 16:24:24 +0000

    Put and delete

--- app/api/Teams/(owner)/modifyTeams/[id]/route.ts
+++ app/api/Teams/(owner)/modifyTeams/[id]/route.ts
@@ -12,4 +12,76 @@ interface params extends Request {
     };
 }
 
+export async function GET(request: Request, params: params) {
+    const id = params.params.id;
 
+    try {
+        const teams = await Teams.findById(id)
+        if(!teams){
+            return new NextResponse("Teams not found",{
+                status:404,
+            });
+        }
+        return new NextResponse(JSON.stringify(teams),{
+            status:200,
+        });
+    } catch (err){
+        console.log(err);
+        return new NextResponse(JSON.stringify(err),{
+            status: 500,
+        });
+    }
+}
+
+
+export async function PUT(request: Request, params: params) {
+    const id = params.params.id;
+    const data = await request.json();
+
+    try{
+        const teams = await Teams.findByIdAndUpdate(id, data, {
+            new: true,
+        });
+
+        if (!teams) {
+            return new NextResponse("Teams not found",{
+                status: 404,
+            });
+        }
+        return new NextResponse(JSON.stringify(teams),{
+            status: 200,
+        });
+    }catch (err) {
+        console.log(err);
+
+        return new NextResponse(JSON.stringify(err),{
+            status:500,
+        });
+    }
+}
+
+export async function DELETE(request: Request, params: params) {
+    const id = params.params.id;
+
+    try{
+        const teams = await Teams.findByIdAndDelete(id);
+
+        if (!teams){
+            return new NextResponse("Teams not found",{
+                status: 404,
+            });
+        }
+
+        return new NextResponse(JSON.stringify(teams),{
+            status: 200,
+        });
+            
+        
+
+    } catch (err){
+        console.log(err);
+        return new NextResponse(JSON.stringify(err),{
+            status: 500,
+        });
+    }
+}
